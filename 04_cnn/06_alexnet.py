a1d365f01f8ee66a1e93e6c3fb66fa84f1f6c479
stxw <1601325@stu.nchu.edu.cn>
2020-11-18 13:55:14 +0800

    add 06_alexnet

--- 04_cnn/06_alexnet.py
+++ 04_cnn/06_alexnet.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import os
-os.environ["MXNET_CUDNN_AUTOTUNE_DEFAULT"] = "0"
 import sys
 import mxnet as mx
 from mxnet import autograd, gluon, init, nd
@@ -34,9 +33,9 @@ for layer in net:
 	x = layer(x)
 	print(layer.name, 'output shape:\t', x.shape)
 
-def try_gpu(gpu_id):
+def try_gpu():
 	try:
-		ctx = mx.gpu(gpu_id)
+		ctx = mx.gpu()
 		_ = nd.zeros((1,), ctx=ctx)
 	except mx.base.MXNetError:
 		ctx = mx.cpu()
@@ -63,14 +62,6 @@ def load_data_fashion_mnist(batch_size, resize=None,
 
 batch_size = 128
 train_iter, test_iter = load_data_fashion_mnist(batch_size, resize=224)
-
-def evaluate_accuracy(data_iter, net, ctx):
-	acc_sum, n = nd.array([0], ctx=ctx), 0
-	for x, y in data_iter:
-		x, y = x.as_in_context(ctx), y.as_in_context(ctx).astype('float32')
-		acc_sum += (net(x).argmax(axis=1) == y).sum()
-		n += y.size
-		return acc_sum.asscalar() / n
 
 def train(net, train_iter, test_iter, batch_size, trainer, num_epochs, ctx=mx.cpu()):
 	print("training on", ctx)
@@ -100,7 +91,7 @@ def train(net, train_iter, test_iter, batch_size, trainer, num_epochs, ctx=mx.cp
 
 lr = 0.01
 num_epochs = 5
-ctx = try_gpu(gpu_id=7)
+ctx = try_gpu()
 net.initialize(force_reinit=True, ctx=ctx, init=init.Xavier())
 trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': lr})
 train(net, train_iter, test_iter, batch_size, trainer, num_epochs, ctx)
